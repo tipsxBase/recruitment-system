165af0c760b6cbc93664ff9b5f2159597c335e8f
zhaowencong <zhaowencong@hexadb.com>
2025-08-07 14:36:27 +0800

    fix:项目重构

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,14 +1,20 @@
-import path from "path";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+import tanstackRouter from "@tanstack/router-plugin/vite";
+import { resolve } from "node:path";
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    tanstackRouter({ autoCodeSplitting: true }),
+    viteReact(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": resolve(__dirname, "./src"),
     },
   },
 });
